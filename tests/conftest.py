e437481748e55470fbe080ee8fc7c1f3df7ee958
Ben <btr1975@gmail.com>
2023-03-21 21:19:59 -0500

    added more tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,3 +2,8 @@ import os, sys
 import pytest
 base_path = os.path.join(os.path.abspath(os.path.dirname(__name__)))
 sys.path.append(os.path.join(base_path))
+
+
+@pytest.fixture
+def log_data_path():
+    return 'tests/data/unit-test.logs'
